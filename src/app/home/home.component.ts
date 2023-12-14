ffe73c911ce5c7feffe0c9c7ebdda97b2c79232c
Dave Budah <davebudah@gmail.com>
2023-12-14 13:24:40 +0200

    contact form removed, footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,8 @@ import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
-  UntypedFormGroup,
   Validators
 } from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
-import {ContactService} from "../services/contact.service";
 import {FeedComponent} from "./projects/feed/feed.component";
 
 @Component({
@@ -26,12 +23,8 @@ export class HomeComponent  implements OnInit {
   apiUrl = '/projects'
 
   FormData!: FormGroup;
-  successMessage: string = ''
-  errorMessage: string= '';
   emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-  constructor(private _formBuilder: FormBuilder,
-              private httpClient: HttpClient,
-              private contactService: ContactService) {}
+  constructor(private _formBuilder: FormBuilder) {}
   ngOnInit(): void {
     // Contact Form
     this.FormData = this._formBuilder.group({
@@ -59,19 +52,4 @@ export class HomeComponent  implements OnInit {
     return this.FormData.get(name)
   }
 
-  onSubmit(FormData: FormData) {
-    if (this.FormData.invalid) {
-      return;
-    }
-    this.contactService.PostMessage(FormData)
-      .subscribe(response => {
-        location.href = 'https://mailthis.to/confirm'
-        console.log(response)
-        console.log(FormData)
-      }, error => {
-        console.warn(error.responseText)
-        console.log({ error })
-      })
-    this.FormData.reset();
-  }
 }
